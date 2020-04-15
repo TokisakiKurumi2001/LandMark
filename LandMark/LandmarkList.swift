e8f5855e1a79c24cdf8c71557eb38d2cfd790c18
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2020-04-15 11:17:15 +0700

    Preview many devices UI Design

--- LandMark/LandmarkList.swift
+++ LandMark/LandmarkList.swift
@@ -23,6 +23,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPad Pro (11-inch)", "iPhone 11 Pro Max"], id: \.self) { deviceName in
+            LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: deviceName))
+            .previewDisplayName(deviceName)
+        }
     }
 }
