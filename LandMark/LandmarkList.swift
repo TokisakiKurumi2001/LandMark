8c6a3fae3d38f6bcf1383373dcb3e7b53fd3c8cf
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2020-04-22 11:42:49 +0700

    Combine state of UI and Toggle with ForEach embedded in List to create the Show Favorite/All

--- LandMark/LandmarkList.swift
+++ LandMark/LandmarkList.swift
@@ -9,11 +9,20 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State var showFavoriteList = true
+    
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                NavigationLink(destination: LandMarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoriteList) {
+                    Text("Favorite only")
+                }
+                ForEach(landmarkData) { landmark in
+                    if !self.showFavoriteList || landmark.isFavorite {
+                        NavigationLink(destination: LandMarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
             }
             .navigationBarTitle(Text("Landmarks"))
@@ -23,10 +32,11 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPad Pro (11-inch)", "iPhone 11 Pro Max"], id: \.self) { deviceName in
-            LandmarkList()
-            .previewDevice(PreviewDevice(rawValue: deviceName))
-            .previewDisplayName(deviceName)
-        }
+//        ForEach(["iPad Pro (11-inch)", "iPhone 11 Pro Max"], id: \.self) { deviceName in
+//            LandmarkList()
+//            .previewDevice(PreviewDevice(rawValue: deviceName))
+//            .previewDisplayName(deviceName)
+//        }
+        LandmarkList()
     }
 }
